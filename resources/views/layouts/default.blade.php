73500e0c8252b1616ac9d5ac61e70dee7d0ed895
nin-kae <renhuaying0401@gmail.com>
2025-05-13 00:29:35 +0900

    完成拆分 _header.blade.php 和 _footer.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,22 +2,17 @@
 <html lang="en">
 <head>
     <title>@yield('title', 'Weibo App') - Laravel blog</title>
-    {{-- vite 是引用 resource 下的 css & js --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/">Lu Stormstout</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
-        </ul>
-    </div>
-</nav>
+@include('layouts._header')
 
 <div class="container">
+
     @yield('content')
+
+    @include('layouts._footer')
+
 </div>
 </body>
 </html>
